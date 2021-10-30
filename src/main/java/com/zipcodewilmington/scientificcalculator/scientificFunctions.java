b8c3f4508ed0ea09e8abb34b3f015a89c99079d4
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2021-10-30 16:56:30 -0400

    chang

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/scientificFunctions.java
@@ -0,0 +1,27 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class scientificFunctions {
+    public static Double sineFunction(Double d) {
+        double sine = 0;
+
+
+
+        return sine;
+    }
+
+    public static Double cosineFunction(Double d) {
+        double cosine = 0;
+
+
+
+        return cosine;
+    }
+
+    public static Double tangentFunction(Double d) {
+        double tangent = 0;
+
+
+
+        return tangent;
+    }
+}
