4a33e1aebc2906f46eb10bbdaa3edc13a98c5716
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-10-31 16:19:32 -0400

    Version fix?

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,9 +1,13 @@
 package com.zipcodewilmington.scientific_calculator;
 
 import com.zipcodewilmington.scientificcalculator.MainApplication;
+import com.zipcodewilmington.scientificcalculator.scientificFunctions;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by leon on 2/9/18.
@@ -12,6 +16,21 @@ public class TestMainApplication {
     public MainApplication testCoreFunctions = new MainApplication();
 
     @Test
+    public void testFactorial() {
+        assertEquals(Double.valueOf(6.0), scientificFunctions.factorialFunction(3.0)); //should be true
+        assertEquals(Double.valueOf(10.0), scientificFunctions.factorialFunction(10.0)); //should be false
+    }
+
+    @Test
+    public void testTrigFunctions() {
+        scientificFunctions testSine = new scientificFunctions();
+
+        String input = "sine";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertEquals(Double.valueOf(-0.5440211108893698),testSine.trigFunctions(10.0));
+    }
+    /*@Test
     public void testAddition() {
         assertEquals(Double.valueOf(5.0), testCoreFunctions.basicoperations("+", 2,3));
         assertEquals(Double.valueOf(1000.0), testCoreFunctions.basicoperations("+", 565,435));
@@ -23,6 +42,7 @@ public class TestMainApplication {
         assertEquals(Double.valueOf(1000.0), testCoreFunctions.basicoperations("-", 2000, 1000));
     }
 
+
     @Test
     public void testMultiplication() {
         //test
@@ -32,7 +52,5 @@ public class TestMainApplication {
     public void testDivision() {
 
     }
-
-    @Test
-    public void
+    */
 }
