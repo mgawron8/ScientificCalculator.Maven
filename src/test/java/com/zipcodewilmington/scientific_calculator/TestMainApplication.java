3920ebd4e0c3aa6a32b506ff480fe891f0a011a1
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-10-31 12:36:08 -0400

    Testing commiting TestJava

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,38 @@
 package com.zipcodewilmington.scientific_calculator;
 
+import com.zipcodewilmington.scientificcalculator.MainApplication;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+
+    @Test
+    public void testAddition() {
+        MainApplication testCoreFunctions = new MainApplication();
+        assertEquals(Double.valueOf(5.0), testCoreFunctions.basicoperations(2,3));
+        assertEquals(Double.valueOf(1000.0), testCoreFunctions.basicoperations(565,435));
+        assertEquals(Double.valueOf(1.0), testCoreFunctions.basicoperations(0.6,0.4));
+    }
+
+    @Test
+    public void testSubtraction() {
+
+    }
+
+    @Test
+    public void testMultiplication() {
+
+    }
+
+    @Test
+    public void testDivision() {
+
+    }
+
+    @Test
+    public void
 }
