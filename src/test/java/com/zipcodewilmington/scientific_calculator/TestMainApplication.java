69194ac2262b767081285265d6199af3a012166b
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-10-31 12:48:28 -0400

    Testing more

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -9,18 +9,18 @@ import static org.junit.Assert.assertEquals;
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+    public MainApplication testCoreFunctions = new MainApplication();
 
     @Test
     public void testAddition() {
-        MainApplication testCoreFunctions = new MainApplication();
-        assertEquals(Double.valueOf(5.0), testCoreFunctions.basicoperations(2,3));
-        assertEquals(Double.valueOf(1000.0), testCoreFunctions.basicoperations(565,435));
-        assertEquals(Double.valueOf(1.0), testCoreFunctions.basicoperations(0.6,0.4));
+        assertEquals(Double.valueOf(5.0), testCoreFunctions.basicoperations("+", 2,3));
+        assertEquals(Double.valueOf(1000.0), testCoreFunctions.basicoperations("+", 565,435));
     }
 
     @Test
     public void testSubtraction() {
-
+        assertEquals(Double.valueOf(5.0), testCoreFunctions.basicoperations(2,3));
+        assertEquals(Double.valueOf(1000.0), testCoreFunctions.basicoperations(565,435));
     }
 
     @Test
